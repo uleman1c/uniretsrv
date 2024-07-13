7a3049abc0477db1bbb6ae81533c2b9b40bf53de
Mihail <uleman@mail.ru>
2024-07-13 15:06:09 +0300

    node service

--- nodeService.js
+++ nodeService.js
@@ -1,10 +1,21 @@
 import { Service } from 'node-windows'
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, 'index.js');
+
+const dirs = __dirname.split(path.sep)
+
+const serviceName = dirs[dirs.length - 1]
 
 // Create a new service object
 var svc = new Service({
-  name:'node uniret',
-  description: 'The node js uniret web server.',
-  script: 'C:\\Users\\Mihail\\Projects\\uniretsrv\\index.js',
+  name:'node ' + serviceName,
+  description: 'The node js ' + serviceName + ' web server.',
+  script: filePath,
   nodeOptions: [
     '--harmony',
     '--max_old_space_size=4096'
